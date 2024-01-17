295bbbb50e10be3c199a38c1210db404ab5720e0
105021113 <zoozootom@gmail.com>
2024-01-17 18:33:21 +0800

    reset vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,12 +4,10 @@
   </div>
 </template>
 
+<script>
+
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
