14d7594ce40a8fe2e441b5bcad1a93effaf6af8a
105021113 <zoozootom@gmail.com>
2024-01-17 18:11:21 +0800

    reset vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,93 +1,10 @@
 <script>
 // import TheWelcome from '../components/TheWelcome.vue'
-  const imgUrl = new URL('../src/assets/image/home/hannah-wei-aso6SYJZGps-unsplash.jpg', import.meta.url).href
-  export default {
-    data() {
-      return {
-        rawHtml: '<p>1</p>',
-        imgUrl: imgUrl,
-        list: [
-          {
-            src: 'home/hannah-wei-aso6SYJZGps-unsplash.jpg',
-            name: 'pic1',
-            price: '150',
-          },
-          {
-            src: 'home/flowstep1.png',
-            name: 'pic2',
-            price: '100',
-          },
-          {
-            src: 'home/products_craft_2.png',
-            name: 'pic3',
-            price: '200',
-          },
-          {
-            src: 'home/flowstep3.png',
-            name: 'pic4',
-            price: '500',
-          }
 
-        ],
-        imgIndex: 1,
-      }
-    },
-    // components:{
-    //   TheWelcome
-    // },
-    methods:{
-      getImageUrl(paths){
-        return new URL(`../assets/image/${paths}`, import.meta.url).href
-      },
-      toggleImgBefore(){
-        if(this.imgIndex!==1){
-          this.imgIndex-= 1;
-        }else{
-          this.imgIndex = 7;
-        }
-      },
-      toggleImgAfter(){
-        //this.imgIndex = (this.imgIndex)%7+1;
-        if(this.imgIndex!==7){
-          this.imgIndex+= 1;
-        }else{
-          this.imgIndex = 1;
-        }
-      }
-    },
-}
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" :src="getImageUrl('home/hannah-wei-aso6SYJZGps-unsplash.jpg')" width="125" height="125" />
-    <img alt="Vue logo" class="logo" :src="getImageUrl('home/flowstep1.png')" width="125" height="125" /> -->
 
-    <!-- <img v-for="(item, index) in list"  :src="getImageUrl(item)" :width="(index+1)*100" alt=""> -->
-
-
-    <div v-for="item in list">
-      <img :src="getImageUrl(item.src)" width='120px' height='120px' alt="">
-      <p>{{ item.name }}</p>
-      <br>
-      <p>價錢 : {{ item.price }}</p>
-    </div>
-    <p v-for="num in 2">
-      <img :src="getImageUrl(`product/${num}.jpg`)" width="100px" alt="">
-      <!-- {{ getImageUrl(`product/${num}.png`) }} -->
-    </p>
-
-    <img :src="getImageUrl(`product/${imgIndex}.jpg`)" width="500px" alt="" >
-    <br>
-    <button @click="toggleImgBefore">上一張</button>
-    <img v-for="num in 7" :src="getImageUrl(`product/${num}.jpg`)" @click="imgIndex = num" width="100px">
-    <!-- <img :src="getImageUrl(`product/${imgIndex}.jpg`)" width="100px" alt=""> -->
-    <button @click="toggleImgAfter">下一張</button>
-    <!-- <p>{{ imgIndex }}</p> -->
-
-
-
-  </header>
 
 </template>
 
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,65 +1,6 @@
-<script>
-//
-  import axios from 'axios';
-  export default {
-    data(){
-      return {
-        search:'',
-        responseData : [],
-        displayData:[],
-        max:1000000000,
-        min:0,
-      }
-    },
-    created() {
-      console.log('嗨');
-      //this.fetchData()
-      this.axiosGetData();
-    },
-    methods: {
-      fetchData(){
-        console.log('api');
-        fetch('https://fakestoreapi.com/products')
-        .then(res=>{
-          return res.json();
-        })
-        .then(json=>{
-            this.responseData=json;
-            console.log(this.responseData);
-        })
-      },
-      axiosGetData(){
-        axios.get('https://fakestoreapi.com/products')
-        .then(res=>{
-          console.log(res.data);
-          this.responseData = res.data
-          this.displayData = res.data
-        })
-      },
-      handleInput(){
-        this.displayData = this.responseData.filter((item)=>{
-          return item.title.includes(this.search)&&item.price>this.min&&item.price<this.max;
-        })
-      }
-    },
-  }
-</script>
 <template>
-  <div class="product">
+  <div class="login">
     <h1>This is an about page</h1>
-    <input type="text" v-model.trim="search" @input="handleInput">
-    <br>
-    <input type="number" v-model="min" @input="handleInput">-
-    <input type="number" v-model="max" @input="handleInput">
-    <!-- {{ responseData }} -->
-
-    <div class="item" v-for="item in displayData">
-      <img :src="item.image" width='100px' alt="">
-      <p>價錢:{{ item.price }}</p>
-      <p>品名:{{ item.title }}</p>
-      <p>描述:{{ item.description }}</p>
-    </div>     <!-- -->
-
   </div>
 </template>
 
