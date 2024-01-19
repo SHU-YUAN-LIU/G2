8bd106091b7330739f7c6eb7741a288dcc276eff
105021113 <zoozootom@gmail.com>
2024-01-19 17:25:27 +0800

    new card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="card">
     <img width="200px" :src="imgSrc">
-<<<<<<< HEAD
-    <p>品名:{{ name }}</p>
-    <p>價錢:{{ price }}</p>
-=======
     <p class="dog">{{ name }}</p>
     <p class="fish">價錢:{{ price }}</p>
->>>>>>> f2dadc6a71a50df5ce3bb5c3cb388c6873c1a4a8
     <div class="content">
       <p>內容:{{ int }}</p>
     </div>
@@ -24,13 +19,6 @@ export default {
   props: ['imgSrc', 'name', 'price', 'int'],
   created() { },
   methods: {
-<<<<<<< HEAD
-  }
-}
-</script>
-
-<style></style>
-=======
 
   }
 }
@@ -54,4 +42,3 @@ export default {
   font-family: noto sans, sans-serif;
 }
 </style>
->>>>>>> f2dadc6a71a50df5ce3bb5c3cb388c6873c1a4a8
