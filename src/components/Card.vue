6b060a2d2200e3939529c02c6172c5598e5036e5
SHU-YUAN-LIU <v2525v2525v2525@gmail.com>
2024-01-19 19:18:33 +0800

    debug product

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="card">
-    <<<<<<< HEAD <div class="card_pic">
+    <div class="card_pic">
       <img width="200px" :src="imgSrc">
-  </div>
-  <p class="title">{{ name }}</p>
-  <p class="price">價錢:{{ price }}</p>
-  =======
-  <img width="200px" :src="imgSrc">
-  <p class="title">{{ name }}</p>
-  <p class="price">價錢:{{ price }}</p>
-  <!-- <div class="content">
-      <p>內容:{{ int }}</p>
-    </div> -->
-  >>>>>>> 130bbafc562e5f487ca16a1e2a6edb3d9ecde520
+    </div>
+    <p class="title">{{ name }}</p>
+    <p class="price">價錢:{{ price }}</p>
+
+    <img width="200px" :src="imgSrc">
+    <p class="title">{{ name }}</p>
+    <p class="price">價錢:{{ price }}</p>
+
   </div>
 </template>
 <script>
