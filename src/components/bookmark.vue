7a0999aba5473de33caabc5a6cff67afc44fb0b9
Andrea <62109841+AndreaChuang@users.noreply.github.com>
2024-01-19 16:56:20 +0800

    新增頁籤

---
+++ src/components/bookmark.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="bookmark">
+        <ul>
+            <li v-for="(tab, index) in tabs" :class="{ active: currentIndex == index }" @click="currentIndex = index">
+                {{ tab }}
+            </li>
+        </ul>
+        <ul>
+            <li v-show="currentIndex == 0">我們推出三大關鍵政策方向。首先，...</li>
+            <li v-show="currentIndex == 1">為了將台灣更深入地融入區域經濟，我們推出三大關鍵政策方向。首先，...</li>
+            <li v-show="currentIndex == 2">我們推出三大關鍵政策方向。首先，...</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentIndex: 0,
+            tabs: [
+                'tab1', 'tab2', 'tab3'
+            ]
+        }
+    },
+    props: ['content'],
+    created() { },
+    methods: {
+    }
+}
+</script>
+
+<style>
+/* 頁籤 */
+.bookmark ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 25px;
+
+    li {
+        margin-right: 25px;
+        padding: 7px;
+        cursor: pointer;
+    }
+
+}
+
+.active {
+    background-color: black;
+    color: #fff;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,90 +1,105 @@
 <template>
-<!-- banner -->
-<div class="banner">
-  <button> &lt </button>
-  <ul>
-    <li v-for = "banner in bannerList">
-      <img :src="getImageUrl(banner)" alt="">
-    </li>
-  </ul>
-  <button>></button>
-</div>
-
-
-
-
-
-
-
-
-
-<!-- 捐款 -->
-
-<div class="donate_container">
- 
-  <img v-for="donateImg in donateList" :src="getImageUrl(donateImg)" alt="" class="icon">
-  <div>
-    <h2>小額捐款, 作伙相挺</h2>
-    <div class="donate_content">
-      <img src="../assets/image/home/donate_img.png" alt="">
-      <p>2024 總統大選，面對這光榮民主的一戰，我們需要更多社會支持，邀請大家加入 Team Taiwan，一起挺台灣！</p>
-    </div>
+  <!-- banner -->
+  <div class="banner">
+    <button> &lt </button>
+    <ul>
+      <li v-for="banner in bannerList">
+        <img :src="getImageUrl(banner)" alt="">
+      </li>
+    </ul>
+    <button>></button>
   </div>
 
+  <!-- 頁籤 -->
+  <!-- <div class="bookmark">
+    <ul>
+      <li v-for="(tab, index) in tabs" :class="{ isactive: currentIndex == index }" @click="currentIndex = index">
+        {{ tab }}
+      </li>
+    </ul>
+    <ul>
+      <li v-show="currentIndex == 0">我們推出三大關鍵政策方向。首先，...</li>
+      <li v-show="currentIndex == 1">為了將台灣更深入地融入區域經濟，我們推出三大關鍵政策方向。首先，...</li>
+      <li v-show="currentIndex == 2">我們推出三大關鍵政策方向。首先，...</li>
+    </ul>
+  </div> -->
+  <bookmark />
 
-<!-- 頁面路由route渲染的位置 -->
-<RouterView />
-</div>
+  <!-- 捐款  -->
 
+  <div class=" donate_container">
+
+    <img v-for="donateImg in donateList" :src="getImageUrl(donateImg)" alt="" class="icon">
+    <div>
+      <h2>小額捐款, 作伙相挺</h2>
+      <div class="donate_content">
+        <img src="../assets/image/home/donate_img.png" alt="">
+        <p>2024 總統大選，面對這光榮民主的一戰，我們需要更多社會支持，邀請大家加入 Team Taiwan，一起挺台灣！</p>
+      </div>
+    </div>
+
+
+    <!-- 頁面路由route渲染的位置 -->
+    <RouterView />
+  </div>
 </template>
 
 <script>
-
+import bookmark from '../components/bookmark.vue'
 export default {
   data() {
     return {
-      bannerList:[
+      bannerList: [
         'home/banner_1.png',
         'home/banner_2.png',
       ],
-      donateList:[
+      donateList: [
         'home/donate_icon_1.png',
         'home/donate_icon_2.png',
         'home/donate_icon_3.png',
       ],
-    
     }
   },
   methods: {
     // 改用函式, 動態抓取圖片路徑
     // 參數為圖片路徑的最後一層子資料夾/檔名.副檔名
 
-    getImageUrl(paths){
+    getImageUrl(paths) {
       return new URL(`../assets/image/${paths}`, import.meta.url).href
     },
-   
   },
   mounted() {
-    
+  },
+  computed: {
+    current_tab_component() {
+      return this.currentTab + "_content";
+    }
+  },
+  components: {
+    bookmark,
   },
 }
 </script>
 
+
+
+
 <style>
-.banner{
+.banner {
   overflow: hidden;
   position: relative;
 }
-.banner>ul{
+
+.banner>ul {
   display: flex;
 }
 
-.banner>button{
+.banner>button {
   width: 80px;
   height: 80px;
   border-radius: 50%;
   border: 2px solid #fff;
-  background-color: rgba(255,255,255,.5);
+  background-color: rgba(255, 255, 255, .5);
   backdrop-filter: blur(5px);
   position: absolute;
   top: 0;
@@ -93,72 +108,73 @@ export default {
   font-size: 40px;
 }
 
-.banner>button:first-child{
+.banner>button:first-child {
   left: 60px;
 }
 
-.banner>button:last-child{
+.banner>button:last-child {
   right: 60px;
 }
 
-.donate_container{
+.donate_container {
   width: 100%;
   height: 860px;
   padding-top: 280px;
   background-color: #d4ff1f;
   position: relative;
   border-radius: 10px;
-  }
-  
-  .donate_container h2{
-    font-size: 65px;
-    height: 130px;
-    text-align: center;
-  }
+}
 
-  .donate_container p{
-    font-size: 24px;
-    width: 600px;
-    margin: auto;
-  }
+.donate_container h2 {
+  font-size: 65px;
+  height: 130px;
+  text-align: center;
+}
 
-
-  .donate_content{
-    width: 1200px;
-    height: 300px;
-    background-color: #fff;
-    margin: auto;
-    border-radius: 10px;
-    display: flex;
-  }
+.donate_container p {
+  font-size: 24px;
+  width: 600px;
+  margin: auto;
+}
 
 
-  .donate_container>img{
-    position: absolute;
-  }
+.donate_content {
+  width: 1200px;
+  height: 300px;
+  background-color: #fff;
+  margin: auto;
+  border-radius: 10px;
+  display: flex;
+}
 
-  .donate_container>img:nth-child(1){
-    top: 60px;
-    left: 0;
-    right: 0;
-    margin: auto;
-    width: 200px;
-    height: 200px;
-  }
-  .donate_container>img:nth-child(2){
-    right: 0;
-    bottom: 60px;
-    margin: auto;
-    width: 220px;
-    height: 220px;
-  }
-  .donate_container>img:nth-child(3){
-    left: -100px;
-    top: 0;
-    bottom: 0;
-    margin: auto;
-    width: 250px;
-    height: 250px;
-  }
 
+.donate_container>img {
+  position: absolute;
+}
+
+.donate_container>img:nth-child(1) {
+  top: 60px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 200px;
+  height: 200px;
+}
+
+.donate_container>img:nth-child(2) {
+  right: 0;
+  bottom: 60px;
+  margin: auto;
+  width: 220px;
+  height: 220px;
+}
+
+.donate_container>img:nth-child(3) {
+  left: -100px;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: 250px;
+  height: 250px;
+}
 </style>
