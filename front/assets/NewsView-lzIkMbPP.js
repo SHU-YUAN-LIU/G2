5943646ae25c98390c5087388b880ac08aaf6f09
ubudubud <ubud301@hotmail.com>
2024-01-20 15:40:04 +0800

    新增front、public資料夾

---
+++ front/assets/NewsView-lzIkMbPP.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o,a as t}from"./index-dYQ3rFTJ.js";const c={},a={class:"About"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return o(),s("div",a,_)}const f=e(c,[["render",r]]);export{f as default};
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+
+// })
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production'
+  ? '/chd104/ingrid/front/'
+  : '/',
+  build: {
+    outDir: 'front'
+  },
   plugins: [
     vue(),
   ],
@@ -12,6 +31,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-
+  }
 })
