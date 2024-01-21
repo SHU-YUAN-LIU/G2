2cd905b33a52de7d0c5ae7769932be8a0cf1055e
SHU-YUAN-LIU <v2525v2525v2525@gmail.com>
2024-01-22 02:46:47 +0800

    contect

--- src/components/Bread.vue
+++ src/components/Bread.vue
@@ -1,27 +1,22 @@
 <template>
     <div class="bread">
-        <a>首頁 / 最新消息</a>
+        <ul>
+            <li>
+                <RouterLink to="/">首頁</RouterLink> / {{ page }}
+            </li>
+        </ul>
+
     </div>
-    <!-- <div class="bread">首頁 / 最新消息</div> -->
 </template>
 <script>
-
 export default {
-    components: {
-
-    },
     data() {
         return {
 
         }
     },
-    props: ['imgSrc', 'name', 'price', 'int', 'num'],
-    created() {
+    props: ['page',],
 
-    },
-    methods: {
-
-    }
 }
 </script>
 <style lang="scss"></style>
