841cea375af04125c03a27ecb7df8424aa930556
Andrea <62109841+AndreaChuang@users.noreply.github.com>
2024-01-22 09:35:13 +0800

    新增跑馬燈功能 + 新增選舉資訊頁面

---
+++ src/components/Marquee.vue
@@ -0,0 +1,11 @@
+<template>
+    <Vue3Marquee>
+        <span v-for="(word, index) in helloArray" :key="index">
+            {{ word }}
+        </span>
+    </Vue3Marquee>
+</template>
+
+<script setup>
+const helloArray = ['這是一場改變政治文化的社會運動，投給劉緯育，投給自己的未來。']
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Vue3Marquee from 'vue3-marquee'
 
 import App from './App.vue'
 import router from './router'
+
+
 
 import "./assets/scss/style.scss"
 
@@ -11,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Vue3Marquee)
 // 渲染在index.html裡面的div#app
 app.mount('#app')
