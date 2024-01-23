a6899a5d5d312aeb555975a1288cc60411656010
wendychen0731 <sun609099@gmail.com>
2024-01-23 19:15:38 +0800

    捐款頁面切版

--- src/views/DonateView_page.vue
+++ src/views/DonateView_page.vue
@@ -1,177 +1,153 @@
 <template>
-  <div class="donate_page">
-    <div class="donate_page_banner">
-      <img src="../assets/image/donate/bg_banner.png" alt="">
-      <h1>我要捐款</h1>
-    </div>
-
-    <breadCrumbs />
-
-    <div class="donate_page_main">
-      <!-- 標題 -->
-      <div class="donate_page_title">
-        <img src="../assets/image/donate/icon_info.png">
-        <h2>我要捐款</h2>
-      </div>
-<<<<<<< HEAD
-
-=======
-      
-
-      <!-- 主要內容區塊 -->
->>>>>>> 32
-      <div class="donate_page_content">
-        <!-- 基本資料 -->
-        <div class="donate_page_info">
-          <h4>捐款基本資料</h4>
-          <ul>
-            <li>真實姓名: <span>吳聰明</span></li>
-            <li>身分證字號: <span>H224384755</span></li>
-            <li>出生年月日: <span>2024/01/01</span></li>
-          </ul>
-          <p>(依政治獻金法規定須年滿20歲才能捐款)</p>
-        </div>
-
-        <!-- 捐款單位 -->
-        <div class="donate_page_unit">
-          <h4>捐款指定單位</h4>
-          <p>單位類別：中央黨部</p>
-        </div>
-
-        <!-- 聯絡資訊 -->
-        <div class="donate_page_contact">
-          <h4>聯絡資訊</h4>
-          <input type="checkbox" id="donate_pageInfo"><label for="donate_pageInfo">以下自動帶入會員資料</label>
-          <div class="donate_page_email">
-            <label for="donate_pageMail">電子信箱 <span>*</span></label>
-            <input type="email" id="donate_pageMail" placeholder="請輸入您的電子信箱">
-          </div>
-          <div class="donate_page_phone">
-            <label for="donate_pagePhone">行動電話 <span>*</span></label>
-            <input type="tel" id="donate_pagePhone" placeholder="請輸入您的行動電話">
-          </div>
-        </div>
-
-        <!-- 捐款方式 -->
-        <div class="donate_page_method">
-          <h4>捐款方式</h4>
-          <p>請選擇付款方式</p>
-          <ul>
-            <li>
-              <p>線上刷卡</p>
-              <img src="../assets/image/donate/icon_card.png" alt="">
-            </li>
-            <li>
-              <p>行動支付</p>
-              <img src="../assets/image/donate/icon_mobilePay.png" alt="">
-            </li>
-            <li>
-              <p>Web ATM</p>
-              <img src="../assets/image/donate/icon_atm.png" alt="">
-            </li>
-          </ul>
-        </div>
-
-        <!-- 捐款金額 -->
-        <div class="donate_page_amount">
-          <h4>捐贈金額</h4>
-          <ul>
-            <li>
-              <p class="donate_point">3<span>點</span></p>
-              <div class="donate_amount_item">
-                <p>開路先鋒</p>
-                <p>展現氣勢,壯大團隊自信態度</p>
-              </div>
-              <p class="donate_amount_text">$300</p>
-            </li>
-            <li>
-<<<<<<< HEAD
-              <p class="donate_point">10<span>點</span></p>
-              <div class="donate_amount_item">
-                <p>快速直球</p>
-                <p>正面對決,任何挑戰都無所畏懼</p>
-              </div>
-              <p>$1,000</p>
-            </li>
-            <li>
-              <p class="donate_point">50<span>點</span></p>
-              <div class="donate_amount_item">
-                <p>重砲打者</p>
-                <p>長程火炮,創造大局拉開距離</p>
-              </div>
-              <p>$5,000</p>
-            </li>
-            <li>
-              <p class="donate_point">100<span>點</span></p>
-              <div class="donate_amount_item">
-                <p>王牌投手</p>
-                <p>一夫當闊,掌握賽場順利瞬間</p>
-              </div>
-              <p>$10,000</p>
-            </li>
-            <li>
-              <p class="donate_point">300<span>點</span></p>
-              <div class="donate_amount_item">
-                <p>冠軍教練</p>
-                <p>運籌帷幄,打造戰力最強陣容</p>
-              </div>
-              <p>$30,000</p>
-            </li>
-            <li>
-              <p class="donate_point">500<span>點</span></p>
-              <div class="donate_amount_item">
-                <p>民主MVP</p>
-                <p>攻守俱佳.震懾全場贏得冠軍</p>
-              </div>
-              <p>$50,000</p>
-=======
-                <p class="donate_point">10<span>點</span></p>
-                <div class="donate_amount_item">
-                  <p>快速直球</p>
-                  <p>正面對決,任何挑戰都無所畏懼</p>
-                </div>
-                <p class="donate_amount_text">$1,000</p>
-            </li>
-            <li>
-                <p class="donate_point">50<span>點</span></p>
-                <div class="donate_amount_item">
-                  <p>重砲打者</p>
-                  <p>長程火炮,創造大局拉開距離</p>
-                </div>
-                <p class="donate_amount_text">$5,000</p>
-            </li>
-            <li>
-                <p class="donate_point">100<span>點</span></p>
-                <div class="donate_amount_item">
-                  <p>王牌投手</p>
-                  <p>一夫當闊,掌握賽場順利瞬間</p>
-                </div>
-                <p class="donate_amount_text">$10,000</p>
-            </li>
-            <li>
-                <p class="donate_point">300<span>點</span></p>
-                <div class="donate_amount_item">
-                  <p>冠軍教練</p>
-                  <p>運籌帷幄,打造戰力最強陣容</p>
-                </div>
-                <p class="donate_amount_text">$30,000</p>
-            </li>
-            <li>
-                <p class="donate_point">500<span>點</span></p>
-                <div class="donate_amount_item">
-                  <p>民主MVP</p>
-                  <p>攻守俱佳.震懾全場贏得冠軍</p>
-                </div>
-                <p class="donate_amount_text">$50,000</p>
->>>>>>> 32
-            </li>
-          </ul>
-          <button class="donate_detail">了解進補點數</button>
-        </div>
-      </div>
-      <RouterLink to="/donate/page/confirm"><button class="donate_page_next">下一步,捐款資料 →</button></RouterLink>
-    </div>
+<div class="donate_page">
+  <div class="donate_page_banner">
+    <img src="../assets/image/donate/bg_banner.png" alt="">
+    <h1>我要捐款</h1>
   </div>
+
+  <breadCrumbs />
+
+  <div class="donate_page_main">
+    <!-- 標題 -->
+    <div class="donate_page_title">
+      <img src="../assets/image/donate/icon_info.png">
+      <h2>我要捐款</h2>
+    </div>
+    <div class="donate_page_content">
+      <!-- 基本資料 -->
+      <div class="donate_page_info">
+        <h4>捐款基本資料</h4>
+        <ul>
+          <li>真實姓名: <span>吳聰明</span></li>
+          <li>身分證字號: <span>H224384755</span></li>
+          <li>出生年月日: <span>2024/01/01</span></li>
+        </ul>
+        <p>(依政治獻金法規定須年滿20歲才能捐款)</p>
+      </div>
+
+      <!-- 捐款單位 -->
+      <div class="donate_page_unit">
+        <h4>捐款指定單位</h4>
+        <p>單位類別：中央黨部</p>
+      </div>
+
+      <!-- 聯絡資訊 -->
+      <div class="donate_page_contact">
+        <h4>聯絡資訊</h4>
+        <input type="checkbox" id="donate_pageInfo"><label for="donate_pageInfo">以下自動帶入會員資料</label>
+        <div class="donate_page_email">
+          <label for="donate_pageMail">電子信箱 <span>*</span></label>
+          <input type="email" id="donate_pageMail" placeholder="請輸入您的電子信箱">
+        </div>
+        <div class="donate_page_phone">
+          <label for="donate_pagePhone">行動電話 <span>*</span></label>
+          <input type="tel" id="donate_pagePhone" placeholder="請輸入您的行動電話">
+        </div>
+      </div>
+
+      <!-- 捐款方式 -->
+      <div class="donate_page_method">
+        <h4>捐款方式</h4>
+        <p>請選擇付款方式</p>
+        <ul>
+          <li>
+            <p>線上刷卡</p>
+            <img src="../assets/image/donate/icon_card.png" alt="">
+          </li>
+          <li>
+            <p>行動支付</p>
+            <img src="../assets/image/donate/icon_mobilePay.png" alt="">
+          </li>
+          <li>
+            <p>Web ATM</p>
+            <img src="../assets/image/donate/icon_atm.png" alt="">
+          </li>
+        </ul>
+      </div>
+
+      <!-- 捐款金額 -->
+      <div class="donate_page_amount">
+        <h4>捐贈金額</h4>
+        <ul>
+          <li>
+            <p class="donate_point">3<span>點</span></p>
+            <div class="donate_amount_item">
+              <p>開路先鋒</p>
+              <p>展現氣勢,壯大團隊自信態度</p>
+            </div>
+            <p class="donate_amount_text">$300</p>
+          </li>
+          <li>
+            <p class="donate_point">10<span>點</span></p>
+            <div class="donate_amount_item">
+              <p>快速直球</p>
+              <p>正面對決,任何挑戰都無所畏懼</p>
+            </div>
+            <p class="donate_amount_text">$1,000</p>
+          </li>
+          <li>
+            <p class="donate_point">50<span>點</span></p>
+            <div class="donate_amount_item">
+              <p>重砲打者</p>
+              <p>長程火炮,創造大局拉開距離</p>
+            </div>
+            <p class="donate_amount_text">$5,000</p>
+          </li>
+          <li>
+            <p class="donate_point">100<span>點</span></p>
+            <div class="donate_amount_item">
+              <p>王牌投手</p>
+              <p>一夫當闊,掌握賽場順利瞬間</p>
+            </div>
+            <p class="donate_amount_text">$10,000</p>
+          </li>
+          <li>
+            <p class="donate_point">300<span>點</span></p>
+            <div class="donate_amount_item">
+              <p>冠軍教練</p>
+              <p>運籌帷幄,打造戰力最強陣容</p>
+            </div>
+            <p class="donate_amount_text">$30,000</p>
+          </li>
+          <li>
+            <p class="donate_point">500<span>點</span></p>
+            <div class="donate_amount_item">
+              <p>民主MVP</p>
+              <p>攻守俱佳.震懾全場贏得冠軍</p>
+            </div>
+            <p class="donate_amount_text">$50,000</p>
+          </li>
+        </ul>
+        <button class="donate_detail">了解進補點數</button>
+      </div>
+
+      <!-- 自訂金額區塊 -->
+      <div class="donate_page_inputAmount">
+        <p>自訂金額 <span>獲得: 20點</span></p>
+        <div>
+          <table>
+            <tr>
+              <td>新台幣</td>
+              <td><input type="number" placeholder="請自行輸入金額"></td>
+              <td>元</td>
+            </tr>
+          </table>
+        </div>
+        
+        <p>目前選擇方式可捐款金額為新台幣 <span>300 ~ 300,000</span> 元</p>
+      </div>
+      
+      <div class="donate_page_attention">
+        <h4>捐款須知</h4>
+        <input type="checkbox" id="donate_attention">
+        <label for="donate_attention">本人已年滿20歲並同意遵守政治獻金相關法規於線上捐贈政治獻金 (政治獻金法及捐款須知)</label>
+      </div>
+
+    </div>
+
+    <RouterLink to="/donate/page/confirm"><button class="donate_page_next">下一步,捐款資料 →</button></RouterLink>
+  </div>
+</div>
 </template>
 
 
@@ -200,27 +176,14 @@ export default {
 
 <style scoped lang="scss">
 @import '../assets/scss/style.scss';
-
-<<<<<<< HEAD
-.donate_page {
-
-  h4 {
-    margin-bottom: 30px;
-    font-size: $font_size_8;
-    font-family: $font_family_2;
-    font-weight: inherit;
-    color: $orange;
-  }
-
-  .donate_page_banner {
-=======
+.donate_page{
   h4{
         margin-bottom: 30px;
         @include title_4;
         color: $orange;
+        width: 100%;
         }
   .donate_page_banner{
->>>>>>> 32
     width: 100%;
     position: relative;
     display: flex;
@@ -257,7 +220,7 @@ export default {
     button.donate_page_next{
     @include btn_3;
     margin: 90px 0;
-  }
+    }
 
     // 我要捐款的標題
     .donate_page_title {
@@ -266,6 +229,7 @@ export default {
       gap: 20px;
       border-bottom: 1px solid $gray_2;
       height: 80px;
+      width: 100%;
 
       h2 {
         @include title_3;
@@ -281,6 +245,9 @@ export default {
       flex-direction: column;
       gap: 40px;
 
+      input{
+        margin-top: 10px;
+      }
       // 捐款資料內容_會員資料
       .donate_page_info {
 
@@ -368,12 +335,6 @@ export default {
       }
 
       // 捐款資料內容_金額選擇
-<<<<<<< HEAD
-      .donate_page_amount {
-        border: 1px solid #000;
-
-        ul {
-=======
       .donate_page_amount{
         display: flex;
         flex-direction: column;
@@ -382,11 +343,10 @@ export default {
             margin-bottom: 60px;
           }
         ul{
->>>>>>> 32
           display: flex;
           gap: 70px;
           flex-wrap: wrap;
-          justify-content: center;
+          justify-content: space-around;
           align-items: center;
           
           
@@ -397,63 +357,8 @@ export default {
             width: 310px;
             height: 160px;
 
-<<<<<<< HEAD
-          li {
-            border-radius: $border-radius-1;
-            border: 1px solid $gray_3;
-            position: relative;
-            width: 310px;
-            height: 160px;
-
-            div {}
-
-            p:first-child {
-              border-radius: 50%;
-              background: $yellow_2;
-              width: 100px;
-              height: 100px;
-              text-align: center;
-              line-height: 100px;
-              color: $white;
-              position: absolute;
-              top: -50px;
-              left: -50px;
-              @include title_3;
-
-              span {
-                @include title_4;
-                font-size: 18px;
-
-                p.donate_point {
-                  border-radius: 50%;
-                  background: $yellow_2;
-                  width: 100px;
-                  height: 100px;
-                  text-align: center;
-                  line-height: 100px;
-                  color: $white;
-                  position: absolute;
-                  top: -50px;
-                  left: -50px;
-                  letter-spacing: 1px;
-                  @include title_4;
-
-                  span {
-                    @include title_4;
-                    font-size: 18px;
-                  }
-
-                  div {
-                    background: $gray_3;
-                    border: 1px solid #000;
-                  }
-
-
-                }
-              }
-
-=======
-            p.donate_point{
+            // 點數標籤(黃色圓形)
+            .donate_point{
               border-radius: 50%;
               background: $yellow_2;
               width: 110px;
@@ -467,10 +372,8 @@ export default {
               letter-spacing: 1px;
               @include title_4;
               span{
-                @include title_4;
                 font-size: 18px;
               }
-
             }
             .donate_amount_item{
               background: $gray_3;
@@ -485,14 +388,14 @@ export default {
                 text-align: center;
                 margin: 0;
               }
->>>>>>> 32
 
-            }
-
-              p:nth-child(1){
-                @include title_7;
+              p:first-child{
+                font-size: $font_size_7;
               }
+
             }
+
+          }
 
             p.donate_amount_text{
               @include title_4;
@@ -503,8 +406,6 @@ export default {
               line-height: 60px;
             }
           }
-<<<<<<< HEAD
-=======
           
         }
 
@@ -518,11 +419,53 @@ export default {
           height: 40px;
           align-self: flex-end;
         }
->>>>>>> 32
 
+        // 自訂金額區塊
+        .donate_page_inputAmount{
 
+          p:first-child{
+              margin-bottom: 30px;
+              span{
+                color: $orange;
+              }
+            }
+          div{
+            border-radius: $border-radius-1;
+            overflow: hidden;
+
+            table{
+
+              tr{
+                height: 50px;
+                td{
+                  line-height: 50px;
+                }
+
+                td:nth-child(1){
+                  width: 220px;
+                  text-align: center;
+                  background-color: $orange;
+                  color: $white;
+                }
+                td:nth-child(2){
+                  input{
+                    height: 50px;
+                    vertical-align: middle;
+                    margin: 0;
+                    width: 910px;
+                  }
+                }
+                td:nth-child(3){
+                  width: 70px;
+                  text-align: center;
+                  background-color: $orange;
+                  color: $white;
+                }
+              }
+            }
+
+          }
         }
-      }
     }
   }
 }
