49f9d92f2c784626bbcde73bd802988750eb77a3
ax013944 <ax013944@gmail.com>
2024-01-31 23:20:44 +0800

    fixed green background

--- src/components/Background_green.vue
+++ src/components/Background_green.vue
@@ -10,24 +10,48 @@
 .background {
     position: relative;
     bottom: 0;
-    z-index: 2;
+    z-index: 0;
 }
 
 .triangle {
     width: 0;
     height: 0;
-    border-left: 100vw solid transparent;
+    border-left: var(--body-width) solid transparent;
     border-bottom: 20vw solid #D4FF1F;
 }
 
 .rectangle {
     background-color: #D4FF1F;
-    width: 100vw;
 }
 </style>
 
 <script>
 export default {
+    data() {
+        return {
+            resizewidth: "",
+        };
+    },
     props: { height: String },
+    computed: {
+        bodyWidth() {
+            return document.documentElement.offsetWidth + 'px';
+        }
+    },
+    mounted() {
+        document.documentElement.style.setProperty('--body-width', this.bodyWidth);
+        // 监听窗口大小改变事件
+        window.addEventListener('resize', this.resizebody);
+    },
+    methods: {
+        resizebody() {
+            this.resizewidth = document.documentElement.offsetWidth + 'px';
+            document.documentElement.style.setProperty('--body-width', this.resizewidth);
+        }
+    },
+    beforeDestroy() {
+        // 在组件销毁前移除事件监听器，以防止内存泄漏
+        window.removeEventListener('resize', this.resizebody);
+    }
 }
 </script>
