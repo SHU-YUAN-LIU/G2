8ea14a55ce11064055c420fe5fa72eec8e3f8d4b
Andrea <62109841+AndreaChuang@users.noreply.github.com>
2024-02-21 11:35:31 +0800

    調整「了解進補點數」的style

--- src/views/DonateView_page.vue
+++ src/views/DonateView_page.vue
@@ -288,9 +288,10 @@ export default {
         this.$router.push('/donate/page/confirm');
         if (this.amount_input) {
           localStorage.setItem('donateAmount', this.amount_input);
-        } else if (this.currentIndex_amount !== -1) {
-          localStorage.setItem('donateAmount', this.donateAmount[this.currentIndex_amount].amount);
         }
+        // else if (this.currentIndex_amount !== -1) {
+        //   localStorage.setItem('donateAmount', this.donateAmount[this.currentIndex_amount].amount);
+        // }
       }
     },
 
