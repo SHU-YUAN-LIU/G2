2a77ee1e9cb5031bc96bb2342942b05364d591a6
Andrea <62109841+AndreaChuang@users.noreply.github.com>
2024-02-23 13:01:45 +0800

    新增地圖動態pin

--- src/components/mapbox.vue
+++ src/components/mapbox.vue
@@ -5,17 +5,23 @@
 </template>
   
 <script>
+import axios from 'axios';
 
 export default {
+    data() {
+        return {
+            map: null,
+            locations: [],
+        }
+    },
     mounted() {
         mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmVhMDIwOSIsImEiOiJjbHNpczk3cWsyZXV0MmxvNXcydjJwaTFwIn0.G2_Hwk0DPeqNnuq3Wr4mDw';
         this.map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/andrea0209/clsg0qbug005m01rbabj37ap4',
-            // center: [121.2990, 24.9917],
             zoom: 3,
         });
 
+        //  鎖定在台灣+顯示新增的pin
         this.map.on('load', () => {
             this.map.flyTo({
                 center: [123, 24],
@@ -23,48 +29,50 @@ export default {
                 speed: 0.9,
             });
 
+            this.locations.forEach(location => {
+                let coordinates = [location.longitude, location.lattitude];
+                this.addPin(coordinates, location);
+            });
+
         });
-
-
-        this.map.on('click', 'map-pin', (e) => {
-            console.log(e)
-            const coordinates = e.lngLat.toArray();//提出經緯度
-            const name = e.features[0].properties.place_name;
-            // 創建一個 Popup
-            new mapboxgl.Popup()
-                .setLngLat(coordinates) // 欲顯示的位置
-                .setHTML(`<h2>${name}</h2>`)  //欲顯示的內容
-                .addTo(this.map);
-
-            this.$emit('showLocationInfo', { name });
-        });
-
-        this.map.on('click', (e) => {
-            const coordinates = e.lngLat.toArray();
-            const name = `New Pin at ${coordinates[0]}, ${coordinates[1]}`;
-
-            // 動態添加新的 Pin
-            this.addPin(coordinates, name);
-
-            // 創建一個 Popup
-            new mapboxgl.Popup()
-                .setLngLat(coordinates)
-                .setHTML(`<h2>${name}</h2>`)
-                .addTo(this.map);
-
-            this.$emit('showLocationInfo', { name });
-        });
-
-
     },
     methods: {
-        addPin(coordinates, name) {
-            // 在地圖上動態添加新的 Pin
-            new mapboxgl.Marker()
+        // 動態新增pin
+        addPin(coordinates, location) {
+            const el = document.createElement('div');
+            el.className = 'custom-marker';
+            el.style.backgroundImage = `url('https://docs.mapbox.com/mapbox-gl-js/assets/cat.png')`;
+            el.style.width = '50px';
+            el.style.height = '50px';
+            el.style.backgroundSize = 'cover';
+            el.style.borderRadius = '50%';
+
+            // 貓貓pin放到地圖上
+            var newMarker = new mapboxgl.Marker(el)
                 .setLngLat(coordinates)
-                .setPopup(new mapboxgl.Popup().setHTML(`<h2>${name}</h2>`))
+                .setPopup(new mapboxgl.Popup().setHTML(`<h2>${location.address}</h2>`))
                 .addTo(this.map);
+
+            // Marker點擊事件
+            newMarker.getElement().addEventListener('click', () => {
+                this.$emit('showLocationInfo', { campaign_name: location.campaign_name });
+            });
         },
+
+        // 串資料
+        getPin() {
+            axios.get(`${import.meta.env.VITE_PHP_URL} /front_campaign.php`)
+                .then(response => {
+                    console.log(response.data)
+                    this.locations = response.data.campaign;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+    },
+    created() {
+        this.getPin();
     },
 }
 </script>
